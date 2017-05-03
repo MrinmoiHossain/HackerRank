c7fef2facccd31d52911ab2a9d9d4960ffcc553f
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-03 22:28:17 +0600

    hacker rank

---
+++ 30 Days of Code Challenges/Day21-Generics.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+template<typename T>void printArray(vector<T> n){
+    for(T i: n)
+        cout << i << endl;
+}
+
+int main(void)
+{
+    int n;
+	cin >> n;
+
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}
