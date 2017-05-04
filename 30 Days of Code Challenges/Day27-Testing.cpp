8671df4d3f822b2222e4a6c96f069dd7d9213ff7
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-04 08:27:18 +0600

    hacker rank

---
+++ 30 Days of Code Challenges/Day27-Testing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << 5 << endl;
+    cout << 3 << " " << 3 << endl;
+    cout << -1 << " " << 0 << " "  << 5 << endl;
+    cout << 4 << " " << 2 << endl;
+    cout << 0 << " " << -1 << " " << 2 << " " << 1 << endl;
+    cout << 5 << " " << 3 << endl;
+    cout << -7 << " " << 0 << " " << 9 << " " << 8 << " " << 7 << endl;
+    cout << 6 << " " << 2 << endl;
+    cout << 0 << " " << -1 << " " << 2 << " " << 1 << " " << 4 << " " << 8 << endl;
+    cout << 7 << " " << 3 << endl;
+    cout << -1 << " " << 0 << " " << 8 << " " << 9 << " " << 8 << " " << 7 << " " << 5 << endl;
+
+    return 0;
+}
