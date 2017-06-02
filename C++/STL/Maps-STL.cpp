642659667890a042e9daf55962e1454c508985ce
MrinmoiHossain <mokit.aust@gmail.com>
2017-06-02 15:06:27 +0600

    algorithms

---
+++ C++/STL/Maps-STL.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    string s;
+    map<string, int> m;
+    map<string, int>::iterator it;
+    int t, val;
+    for(int i = 1; i <= T; i++){
+        cin >> t >> s;
+
+        switch(t)
+        {
+            case 1:
+                cin >> val;
+                m[s] += val;
+                break;
+            case 2:
+                m.erase(s);
+                break;
+            case 3:
+                it = m.find(s);
+                cout << (it != m.end() ? ((*it).second) : 0) << endl;
+        }
+    }
+
+    return 0;
+}
