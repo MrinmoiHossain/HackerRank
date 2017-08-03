2a3ef3d451c3d123f2ef3f83f8dd9782594ab222
MrinmoiHossain <mokit.aust@gmail.com>
2017-08-03 10:32:19 +0600

    hacker rank

---
+++ Java/Introduction/Java Int to String/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution{
+	public static void main(String[] args){
+
+		DoNotTerminate.forbidExit();
+
+		try{
+			Scanner in = new Scanner(System.in);
+			int n = in.nextInt();
+			in.close();
+		}catch(DoNotTerminate.ExitTrappedException e) {
+	   		System.out.println("Unsuccessful Termination!!");
+	  	}
+	}
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate{
+	public static class ExitTrappedException extends SecurityException{
+		private static final long serialVersionUID = 1;
+ 	}
+
+ 	public static void forbidExit(){
+  		final SecurityManager securityManager = new SecurityManager(){
+   		@Override
+	   		public void checkPermission(Permission permission){
+				if(permission.getName().contains("exitVM")){
+		 			throw new ExitTrappedException();
+				}
+	   		}
+  		};
+  
+		System.setSecurityManager(securityManager);
+ 	}
+}
+
