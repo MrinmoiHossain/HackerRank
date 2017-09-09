b0fea5b7af36659ad2990f9f9c78d81ed55a12d2
MrinmoiHossain <mokit.aust@gmail.com>
2017-09-10 01:31:42 +0600

    Set

---
+++ Python/Sets/Introduction to Sets.py
@@ -0,0 +1,8 @@
+def average(array):
+    return sum(set(array)) / len(set(array))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
