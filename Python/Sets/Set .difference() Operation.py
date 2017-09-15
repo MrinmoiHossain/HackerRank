77dc221b103f47f82b50e2281cd75048c5c4c7f9
MrinmoiHossain <mokit.aust@gmail.com>
2017-09-15 13:05:56 +0600

    set difference

---
+++ Python/Sets/Set .difference() Operation.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = set(input().split(" "))
+    
+    m = int(input())
+    b = set(input().split(" "))
+    
+    a = a.difference(b)
+    #a -= b
+    print(len(a))
