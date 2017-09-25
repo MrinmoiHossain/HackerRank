966cb53e418db5aba44573d5a55e1bc849884ada
MrinmoiHossain <mokit.aust@gmail.com>
2017-09-26 03:46:29 +0600

    basic math

---
+++ Mathematics/Fundamentals/MaximumDraws.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    
+    for(int i = 0, n; i < T; i++){
+        cin >> n;
+        
+        cout << n + 1 << endl;
+    }
+
+    return 0;
+}
