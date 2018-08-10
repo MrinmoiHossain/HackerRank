11e0a92d6872395f67c296ed70e73a95ab4688af
MrinmoiHossain <mokit.aust@gmail.com>
2018-08-11 00:36:27 +0600

    python postal code

---
+++ Python/Regex and Parsing/Validating Postal Codes.py
@@ -0,0 +1,9 @@
+import re
+
+regex_integer_in_range = r"^[1-9][\d]{5}$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"
+
+P = raw_input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
