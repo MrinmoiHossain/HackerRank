ef9bbd6e7e1a54975d06bb8fc10105f4facbc0c2
MrinmoiHossain <mokit.aust@gmail.com>
2018-12-02 23:01:24 +0600

    algebra basic

---
+++ Mathematics/Algebra/ComboMeal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+
+    for(int i = 0, a, b, c; i < t; i++){
+        cin >> a >> b >> c;
+
+        cout << a + b - c << endl;
+    }
+
+    return 0;
+}
