c5dd4faf4b26d5ba5dc530f9a5e2845624684beb
MrinmoiHossain <mokit.aust@gmail.com>
2019-01-02 22:30:33 +0600

    delete duplicated linked list

---
+++ Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = NULL;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = NULL;
+            this->tail = NULL;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep){
+    while(node){
+        cout << node->data;
+
+        node = node->next;
+
+        if(node){
+            cout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    SinglyLinkedListNode* node = head;
+    while(node->next != NULL){
+        if(node->data == node->next->data)
+            node->next = node->next->next;
+        else
+            node = node->next;
+    }
+    return head;
+}
+
+int main(void)
+{
+    int t;
+    cin >> t;
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+
+            llist->insert_node(llist_item);
+        }
+
+        SinglyLinkedListNode* llist1 = removeDuplicates(llist->head);
+
+        print_singly_linked_list(llist1, " ");
+        cout << "\n";
+
+        free_singly_linked_list(llist1);
+    }
+
+    return 0;
+}
