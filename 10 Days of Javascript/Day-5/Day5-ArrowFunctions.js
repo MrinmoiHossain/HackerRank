a57bf3e0ada234d1d94188b5dacaa8a668ac2c93
MrinmoiHossain <mokit.aust@gmail.com>
2019-01-11 13:04:46 +0600

    day-5 of javascript

---
+++ 10 Days of Javascript/Day-5/Day5-ArrowFunctions.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function modifyArray(nums) {
+    let arr = [];
+
+    for(const e of nums){
+        if (e % 2 == 0)
+            arr.push(e * 2);
+        else
+            arr.push(e * 3);
+    }
+    return arr;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
---
+++ 10 Days of Javascript/Day-5/Day5-TemplateLiterals.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function sides(literals, ...expressions) {
+    const [a, p] = expressions;
+    const temp = Math.sqrt(p*p - 16*a);
+    const s1 = (p + temp) / 4;
+    const s2 = (p - temp) / 4;
+    
+    return ([s2, s1]);
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
