cd41175c3cde1f5b7a571fa0753201240d94eb17
MrinmoiHossain <mokit.aust@gmail.com>
2019-06-26 12:12:07 +0600

    data structure tree

---
+++ Data Structures/Trees/TreeHeightOfABinaryTree.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root){
+        if(root == NULL)
+            return -1;
+        else
+            return 1 + max(height(root->left), height(root->right));
+    }
+
+}; //End of Solution
+
+int main(void)
+{
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    int height = myTree.height(root);
+
+  	std::cout << height;
+
+    return 0;
+}
